a590770cfe76796b9e7a9ba2ec5a453dfb78becd
Richmond Chang <changrichmond@gmail.com>
2016-06-22 19:49:34 -0700

    Part 1 Ver 1 -  Added folder structure -  Added sprite sheet, split Player and Ground sprites -  Added GameObjects into the world (Player and some Ground)     -  Added CircleCollider2D and RigidBody2D to Player     -  Added BoxCollider2D to the ground -  Added playerController script, let the player jump, attached to player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+        }
+	}
+}
