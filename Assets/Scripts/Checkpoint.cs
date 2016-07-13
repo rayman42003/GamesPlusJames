b8d3cb65a4ddc5c5b61a10fd8f36bf7f6a7275e1
Richmond Chang <changrichmond@gmail.com>
2016-07-12 20:00:26 -0700

    Part 3 Ver 3: -  Added a simple checkpoint system -  Added prefabs for spikes and checkpoint objects -  Added circle collider trigger and checkpoint for the checkpoint -  Added for Checkpoint to modify the LevelManager currentCheckpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class Checkpoint : MonoBehaviour {
+    public LevelManager levelManager;
 
 	// Use this for initialization
 	void Start () {
-	
+        levelManager = FindObjectOfType<LevelManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if(obj.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            levelManager.currentCheckpoint = this.gameObject;
+        }
+    }
 }
