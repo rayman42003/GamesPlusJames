7708376d1e34f57af2d3500b1402a8f708e3c8ea
Richmond Chang <changrichmond@gmail.com>
2016-07-27 22:00:33 -0700

    Part 6 Ver 2: -  Added text for score -  Added script that allows for dynamic modification of the score

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public static int score;
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        score = 0;
+    }
+
+    void Update()
+    {
+        if (score < 0)
+            score = 0;
+
+        text.text = score.ToString("000");
+    }
+
+    public static void AddPoints(int points)
+    {
+        score += points;
+    }
+
+    public static void Reset()
+    {
+        score = 0;
+    }
+}
